78e0478af591d686e079ba3eb30ce91bdbea99b7
Luis Thiago <LThiagoCH@gmail.com>
2021-05-09 21:46:14 -0300

    Adding Menu

--- src/BurgerShop/App.java
+++ src/BurgerShop/App.java
@@ -1,6 +1,8 @@
 package BurgerShop;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import BurgerShop.Model.ExportOrder.Converter;
 import BurgerShop.Model.ExportOrder.ToCSV;
@@ -12,25 +14,120 @@ import BurgerShop.Model.Order.OrderedItems;
 
 public class App {
     public static void main(String[] args) throws IOException {
+        loop();
+    }
 
-        // Show Menu
+    public static void loop() throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         MenuItem showMenu = new ShowMenu();
-        showMenu.print();
-
-        // Get Order
+        ;
         OrderBuilder builder = new OrderBuilder();
+        OrderedItems orderedItems = null;
 
-        OrderedItems orderedItems = builder.prepareOrder();
+        boolean orderStatus = false;
+        int choice;
 
-        // Show Order
-        System.out.println("Pedido em CSV:\n");
+        while (true) {
 
-        Converter csv = new ToCSV();
-        System.out.println(csv.getOrder(orderedItems));
+            if (orderStatus == false) {
 
-        System.out.println("\nPedido em JSON:\n");
+                System.out.println("==================================================\n"
+                        + "|                Select a Option                 |\n"
+                        + "==================================================\n"
+                        + "| 1 - Show Menu                                  |\n"
+                        + "| 2 - Order                                      |\n"
+                        + "|                                                |\n"
+                        + "| 0 - Exit                                       |\n"
+                        + "==================================================\n");
 
-        Converter json = new ToJSON();
-        System.out.println(json.getOrder(orderedItems));
+                choice = Integer.parseInt(br.readLine());
+
+                if (choice == 0) {
+                    break;
+                }
+
+                switch (choice) {
+                    case 1:
+                        showMenu.print();
+
+                        break;
+
+                    case 2:
+                        orderedItems = builder.prepareOrder();
+                        orderStatus = true;
+
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        break;
+                }
+            } else if (orderStatus == true) {
+
+                System.out.println("==================================================\n"
+                        + "|                Select a Option                 |\n"
+                        + "==================================================\n"
+                        + "| 1 - Show Menu                                  |\n"
+                        + "| 2 - Show Order                                 |\n"
+                        + "|                                                |\n"
+                        + "| 0 - Exit                                       |\n"
+                        + "==================================================\n");
+
+                choice = Integer.parseInt(br.readLine());
+
+                if (choice == 0) {
+                    break;
+                }
+
+                switch (choice) {
+                    case 1:
+                        showMenu.print();
+
+                        break;
+
+                    case 2:
+                        System.out.println("==================================================\n"
+                                + "|                Select a Option                 |\n"
+                                + "==================================================\n"
+                                + "| 1 - Show in CSV                                |\n"
+                                + "| 2 - Show in JSON                               |\n"
+                                + "|                                                |\n"
+                                + "| 0 - Exit                                       |\n"
+                                + "==================================================\n");
+
+                        choice = Integer.parseInt(br.readLine());
+
+                        switch (choice) {
+                            case 1:
+                                Converter csv = new ToCSV();
+                                System.out.println(csv.getOrder(orderedItems));
+
+                                break;
+                            case 2:
+                                Converter json = new ToJSON();
+                                System.out.println(json.getOrder(orderedItems));
+
+                                break;
+                            case 0:
+                                break;
+
+                            default:
+                                break;
+                        }
+
+                        break;
+
+                    case 0:
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
--- src/BurgerShop/Model/Order/OrderBuilder.java
+++ src/BurgerShop/Model/Order/OrderBuilder.java
@@ -197,8 +197,6 @@ public class OrderBuilder {
                 break;
         }
 
-        sc.close();
-
         return itemsOrder;
     }
 
